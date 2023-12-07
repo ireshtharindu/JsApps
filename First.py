6669e264082fe014acb164b41e38447a82a41d91
Iresh Tharindu <68737844+ireshtharindu@users.noreply.github.com>
2023-12-07 16:41:54 +0530

    second commit

---
+++ First.py
@@ -0,0 +1,28 @@
+def add(a, b):
+    return a + b
+
+# Function to subtract two numbers
+def subtract(a, b):
+    return a - b
+
+# Function to multiply two numbers
+def multiply(a, b):
+    return a * b
+
+# Function to divide two numbers
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    else:
+        return a / b
+
+# Example usage
+num1 = 10
+num2 = 5
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
+
+#testing 123
